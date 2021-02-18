147a422278727713c3009cd7ed90dce16f227973
Mario Campopiano <mcampopiano92@gmail.com>
2021-02-18 15:15:21 -0600

    changed : to = in models, and from removed _id

--- rareapi/models/comment.py
+++ rareapi/models/comment.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Comment(models.Model):
-    author_id: models.OneToOneField(User, on_delete=models.CASCADE)
-    subject: models.CharField(max_length=25)
-    comment: models.CharField(max_length=250)
-    deleted: models.BooleanField()
+    author= models.OneToOneField(User, on_delete=models.CASCADE)
+    subject= models.CharField(max_length=25)
+    comment= models.CharField(max_length=250)
+    deleted= models.BooleanField()
--- rareapi/models/tag.py
+++ rareapi/models/tag.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Tag(models.Model):
-    author_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
     tag = models.CharField(max_length=25)

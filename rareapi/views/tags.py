17863030583f7dcc209445f5969d2fc9853f6cef
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-02-19 11:01:02 -0600

    added Tags class for viewset with GET POST PUT and DELETE functions and Tag serializer

---
+++ rareapi/views/tags.py
@@ -0,0 +1,95 @@
+"""View module for handling requests about events"""
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.http import HttpResponseServerError
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from rareapi.models import Tag
+
+class Tags(ViewSet):
+
+    def create(self, request):
+        """Handle POST operations for tags
+
+        Returns:
+            Response -- JSON serialized tag instance
+        """
+
+        tag = Tag()
+        tag.tag = request.data["tag"]
+        token = Token.objects.get(user = request.auth.user)
+        tag.author_id = token
+
+        try:
+            tag.save()
+            serializer = TagSerializer(tag, context={'request': request})
+            return Response(serializer.data)
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single tag
+
+        Returns:
+            Response -- JSON serialized tag instance
+        """
+        try:
+            tag = Tag.objects.get(pk=pk)
+            serializer = TagSerializer(tag, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def update(self, request, pk=None):
+        """Handle PUT requests for a tag
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        tag = Tag()
+        tag.tag = request.data["tag"]
+        token = Token.objects.get(user = request.auth.user)
+        tag.author_id = token
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single tag
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            tag = Tag.objects.get(pk=pk)
+            tag.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Tag.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def list(self, request):
+        """Handle GET requests to tags resource
+
+        Returns:
+            Response -- JSON serialized list of tags
+        """
+        # Get the current authenticated user
+        tags = Tag.objects.all()
+
+        serializer = TagSerializer(
+            tags, many=True, context={'request': request})
+        return Response(serializer.data)
+
+class TagSerializer(serializers.ModelSerializer):
+    """JSON serializer for tags"""
+    class Meta:
+        model = Tag
+        fields = ('id', 'author_id', 'tag')

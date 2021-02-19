832579b596ea094b5e41a2f0d3785b0f672c26a8
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-02-19 13:36:52 -0600

    added tag and post urls

--- rare/urls.py
+++ rare/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from rareapi.views import register_user, login_user, Comments, Categories
+from rareapi.views import register_user, login_user, Comments, Categories, Posts, Tags
 from rest_framework import routers
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'comments', Comments, 'comment')
 router.register(r'categories', Categories, 'category')
+router.register(r'post', Posts, 'post')
+router.register(r'tag', Tags, 'tag')
 
 urlpatterns = [
     path('', include(router.urls)),

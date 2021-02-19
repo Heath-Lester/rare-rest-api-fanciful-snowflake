8a0dac1df8c82a5dee23d6239615dfba9c8635f4
Mario Campopiano <mcampopiano92@gmail.com>
2021-02-19 14:12:15 -0600

    Remigrated to catch updates

--- rareapi/views/posts.py
+++ rareapi/views/posts.py
@@ -122,5 +122,5 @@ class PostSerializer(serializers.ModelSerializer):
     """JSON serializer for posts"""
     class Meta:
         model = Post
-        fields = ('id', 'title', 'content', 'post_time', 'image_url', 'approved', 'deleted', 'author', 'category_id')
+        fields = ('id', 'title', 'content', 'post_time', 'image_url', 'approved', 'deleted', 'author', 'category')
         depth = 2

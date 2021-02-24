8ee293b07e7cd4a0d5571b5371cbbefce6ac82ee
Mario Campopiano <mcampopiano92@gmail.com>
2021-02-24 10:23:31 -0600

    migrated the models to include the subscription.

--- rareapi/models/subscription.py
+++ rareapi/models/subscription.py
@@ -3,7 +3,7 @@ from rest_framework.authtoken.models import Token
 
 
 class Subscription(models.Model):
-    follower = models.ForeignKey(Token, on_delete=models.CASCADE)
+    follower = models.ForeignKey(Token, on_delete=models.CASCADE, related_name = "subscriber")
     author = models.ForeignKey(Token, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now=False, auto_now_add=False)
     ended_on = models.DateTimeField(auto_now=False, auto_now_add=False)
